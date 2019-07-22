751a1d1a2be6c43677aa60e6640d520f9557a817
Joshua Crum <joshua.crum@gmail.com>
2019-07-22 13:05:26 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
     @post.title = params[:title]
     @post.description = params[:description]
     @post.save
+    redirect_to_post_path(@post)
   end
 
 end
